135582f8f33a49621a6869eff5bcc59e754e9e8c
Ryan LeCompte <lecompte@gmail.com>
2011-08-24 20:26:55 -0400

    added xml request middleware

---
+++ lib/faraday/request/xml.rb
@@ -0,0 +1,20 @@
+module Faraday
+  class Request
+    class XML < Faraday::Middleware
+      dependency 'active_support/all'
+
+      def initialize(app, options={})
+        @app     = app
+        @options = options
+      end
+
+      def call(env)
+        if env[:method] == :post
+          env[:body] = env[:body].to_xml(:root => env[:body].delete(:root))
+          puts env[:body]
+        end
+        @app.call(env)
+      end
+    end
+  end
+end

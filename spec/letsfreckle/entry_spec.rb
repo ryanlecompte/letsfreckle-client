27cf5d4b63895494f05d36c64d6ede37b46e42f0
Chris Kelly <ckdake@ckdake.com>
2013-02-06 13:23:00 -0500

    Adds 'all pages' support for entries api

--- spec/letsfreckle/entry_spec.rb
+++ spec/letsfreckle/entry_spec.rb
@@ -16,5 +16,19 @@ describe LetsFreckle::Entry do
       first_entry = entries.first
       first_entry.description.should == 'api test description'
     end
+    
+    it "should return all entries on all pages" do
+      LetsFreckle.configure do
+        username "username"
+        account_host "host"
+        token "secret"
+      end
+
+      stub_api_request('entries', {:page => 1})
+      stub_api_request('entries', {:page => 2})
+      stub_api_request('entries', {:page => 3})
+      entries = LetsFreckle::Entry.all(:all_pages => true)
+      entries.size.should == 4
+    end
   end
 end
